8c9b1db2c4fe1e45424a394c75639d317cf7f1e8
Julián Pachón Castrillón <jupachon@unal.edu.co>
2024-05-27 17:41:51 -0500

    feat: Added validation to avoid inserting duplicated registers

--- scripts/load.py
+++ scripts/load.py
@@ -3,8 +3,21 @@ import os
 import urllib
 from sqlalchemy.orm import sessionmaker
 import uuid as uuid_lib
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from classes.ManizalesWeather import ManizalesWeather
+import datetime as datetime_lib
+
+
+def isCurrentRecordNew(session, new_weather_record: ManizalesWeather) -> bool:
+    latest_record_in_db: ManizalesWeather = (
+        session.query(ManizalesWeather)
+        .order_by(desc(ManizalesWeather.datetime))
+        .first()
+    )
+    if latest_record_in_db == None:
+        return True
+    return new_weather_record.datetime > latest_record_in_db.datetime
+
 
 def upload_data(transformed_weather_data):
     AZURE_ODBC_CONNECTION_STRING: str | None = os.getenv("AZURE_ODBC_CONNECTION_STRING")
@@ -17,34 +30,25 @@ def upload_data(transformed_weather_data):
     engine = create_engine(conn_str, echo=True)
     print("connection is ok")
 
-    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Create a new weather record
     new_weather_record = ManizalesWeather(
-        uuid_value = uuid_lib.uuid4(),
-        datetime = transformed_weather_data["datetime"],
-        consulted_at = transformed_weather_data["consulted_at"],
-        temp = transformed_weather_data["temp"],
-        temp_min = transformed_weather_data["temp_min"],
-        temp_max = transformed_weather_data["temp_max"],
-        feels_like = transformed_weather_data["feels_like"],
+        uuid_value=uuid_lib.uuid4(),
+        datetime=transformed_weather_data["datetime"],
+        consulted_at=transformed_weather_data["consulted_at"],
+        temp=transformed_weather_data["temp"],
+        temp_min=transformed_weather_data["temp_min"],
+        temp_max=transformed_weather_data["temp_max"],
+        feels_like=transformed_weather_data["feels_like"],
     )
 
-    # Add the new weather record to the session
+    if not isCurrentRecordNew(session, new_weather_record):
+        print("Current data is not newer than the data existing in the DB. Skiping.")
+        return
+
     session.add(new_weather_record)
 
-    # Commit the session to write the record to the database
     session.commit()
-
-    # Query to check if the insert was successful
-    weather_records = session.query(ManizalesWeather).all()
-
-    # Print the weather records
-    for record in weather_records:
-        print(
-            f"UUID: {record.uuid}, Datetime: {record.datetime}, Consulted At: {record.consulted_at}, Temp: {record.temp}, Temp Min: {record.temp_min}, Temp Max: {record.temp_max}, Feels Like: {record.feels_like}"
-        )
 
     session.close()
