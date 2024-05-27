f75438a2f13b71f46eb4fe63863097c8369f0a98
Julián Pachón Castrillón <jupachon@unal.edu.co>
2024-05-27 14:43:01 -0500

    feat: Added env variable to manage the pipeline execution interval in minutes

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import extract, transform, load
 import datetime
 import schedule
 import time
+import os
 
-
-def main():
-    load_dotenv()
+def etlProcess():
     current_weather_data = extract.extractCurrentWeatherData()
     print(current_weather_data)
     transformed_weather_data: dict[str, float | datetime.datetime] = (
@@ -14,12 +13,23 @@ def main():
     )
     print(f"The transformed data: \n {transformed_weather_data}")
 
-    print("Connection to azure")
     load.upload_data(transformed_weather_data)
 
 
 if __name__ == "__main__":
-    schedule.every(5).seconds.do(main)
+    load_dotenv()
+    PIPELINE_EXECUTION_INTERVAL_IN_MINUTES: str | None = os.getenv('PIPELINE_EXECUTION_INTERVAL_IN_MINUTES')
+    if PIPELINE_EXECUTION_INTERVAL_IN_MINUTES == None:
+        raise Exception('PIPELINE_EXECUTION_INTERVAL_IN_MINUTES can not be empty')
+    
+    try:
+        PIPELINE_EXECUTION_INTERVAL = int(PIPELINE_EXECUTION_INTERVAL_IN_MINUTES)
+    except:
+        raise Exception("There was an error processing the PIPELINE_EXECUTION_INTERVAL_IN_MINUTES env variable. Please only send numbers.")
+        
+    print(f"Sending requests every {PIPELINE_EXECUTION_INTERVAL} minutes")
+    
+    schedule.every(PIPELINE_EXECUTION_INTERVAL).minutes.do(etlProcess)
     while True:
         schedule.run_pending()
         time.sleep(1)
