f159dfef5a941f12edc7efc60a201c897437cc6f
Julián Pachón Castrillón <jupachon@unal.edu.co>
2024-05-28 11:40:31 -0500

    feat: Created module to handle http requests

---
+++ utils/requests_handler.py
@@ -0,0 +1,17 @@
+from typing import Any
+import requests
+from utils import logs
+from logging import Logger
+
+logger: Logger = logs.getLogger()
+
+
+def performRequest(request_url) -> dict[str, Any]:
+    try:
+        response: requests.Response = requests.get(request_url)
+        data: dict[str, Any] = response.json()
+        return data
+    except:
+        message: str = f"Error performing request to the URL: {request_url}"
+        logger.exception(message)
+        raise Exception(message)
