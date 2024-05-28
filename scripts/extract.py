5153bc8f5b353ceb5264b3a6a126a67622f205e8
Julián Pachón Castrillón <jupachon@unal.edu.co>
2024-05-28 12:00:04 -0500

    feat: Implemented logs in the project scripts

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,32 +1,45 @@
+from typing import Any
 import requests
 import os
+from utils import env_variables, requests_handler, logs
+from logging import Logger
+
+logger: Logger = logs.getLogger()
 
 
 def getCoordinatesByZipCode(API_BASE_URL: str, API_KEY: str) -> dict[str, int]:
-    ZIP_CODE: str | None = os.getenv("ZIP_CODE")
-    COUNTRY_CODE: str | None = os.getenv("COUNTRY_CODE")
+    ZIP_CODE: str = env_variables.readEnvVariable("ZIP_CODE")
+    COUNTRY_CODE: str = env_variables.readEnvVariable("COUNTRY_CODE")
+
     request_url: str = (
         f"{API_BASE_URL}/geo/1.0/zip?zip={ZIP_CODE},{COUNTRY_CODE}&appid={API_KEY}"
     )
-    response: requests.Response = requests.get(request_url)
-    data: dict = response.json()
-    print(f'Identified city: {data["name"]}')
-    coordinates: dict[str, int] = {"latitude": data["lat"], "longitude": data["lon"]}
-    return coordinates
+    data: dict[str, Any] = requests_handler.performRequest(request_url)
+
+    message: str = f'Identified city: {data["name"]}'
+    logger.info(message)
+    print(message)
+
+    try:
+        coordinates: dict[str, int] = {
+            "latitude": data["lat"],
+            "longitude": data["lon"],
+        }
+        return coordinates
+    except:
+        message = "Error trying to extract latitude and longitude according to the ZIP_CODE and the COUNTRY_CODE env variables"
+        logger.exception(message)
+        raise Exception(message)
 
 
-def extractCurrentWeatherData() -> dict:
-    API_BASE_URL: str | None = os.getenv("API_BASE_URL")
-    if API_BASE_URL == None:
-        raise Exception("API_BASE_URL can't be empty")
-    API_KEY: str | None = os.getenv("API_KEY")
-    if API_KEY == None:
-        raise Exception("API_KEY can't be empty")
+def extractCurrentWeatherData() -> dict[str, Any]:
+    API_BASE_URL: str = env_variables.readEnvVariable("API_BASE_URL")
+    API_KEY: str = env_variables.readEnvVariable("API_KEY")
     coordinates: dict[str, int] = getCoordinatesByZipCode(API_BASE_URL, API_KEY)
     LATITUDE: int = coordinates["latitude"]
     LONGITUDE: int = coordinates["longitude"]
     request_url: str = (
         f"{API_BASE_URL}/data/2.5/weather?lat={LATITUDE}&lon={LONGITUDE}&appid={API_KEY}"
     )
-    response: requests.Response = requests.get(request_url)
-    return response.json()
+    current_weather_data: dict[str, Any] = requests_handler.performRequest(request_url)
+    return current_weather_data
